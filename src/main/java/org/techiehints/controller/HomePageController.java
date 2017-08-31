6fdbfbc94a9eff7490a155d3a1b7222ad800d3d4
sanndeepg <sanndeepg@gmail.com>
2017-08-31 12:11:40 +0530

    Hello World MVC
    Adding Spring MVC Project

---
+++ src/main/java/org/techiehints/controller/HomePageController.java
@@ -0,0 +1,14 @@
+package org.techiehints.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "samples")
+public class HomePageController {
+
+	@RequestMapping(value = "my/home.page")
+	public String getHomePage ( ) {
+		return "home";
+	}
+}
